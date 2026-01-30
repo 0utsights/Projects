61a2a43c3f6ec3abe98cf18710f05a949102eb5f
0utsights <outsightszs@outlook.com>
2026-01-29 23:29:36 -0500

    adding name, img, type, and desc calls

--- pokedex/pokedex.js
+++ pokedex/pokedex.js
@@ -4,6 +4,8 @@ var pokedex = {}; // map structure -> {1 : {"name" : "bulbasaur", "img" : url, "
 
 window.onload = async function() {
     getPokemon(1);
+
+    console.log(pokedex);
 }
 
 async function getPokemon(num) {
@@ -12,5 +14,16 @@ async function getPokemon(num) {
     let res = await fetch(url);
     let pokemon = await res.json;
     console.log(pokemon);
+    // console.log(pokemon)
 
+    let pokemonName = pokemon["name"];
+    let pokemonType = pokemon["types"];
+    let pokemonImg = pokemon["sprites"]["front_default"]; // using later in a different project for ascii image with pokemon sprites in terminal
+
+    res = await fetch(pokemon['species']['url']);
+    let pokemonDesc = await res.json();
+
+    pokemonDesc = pokemonDesc["flavor_text_entries"][9]["flavor_text"]
+
+    pokedex[num] = {"name" : pokemonName, "img" : pokemonImg, "types" : pokemonType, "desc" : pokemonDesc}
 }
